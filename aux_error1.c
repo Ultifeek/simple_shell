012ff0ecaa5d40fc0f4635cb32b0d46f5b0035bc
Ultifeek <ultifeek1@gmail.com>
2023-04-25 10:59:25 +0000

    update commit

--- aux_error1.c
+++ aux_error1.c
@@ -10,7 +10,7 @@
 char *error_env(data_shell *datash)
 {
 	int length;
-	char *error, *ver_str,*msg;
+	char *error, *ver_str, *msg;
 
 	ver_str = aux_itoa(datash->counter);
 	msg = ": Unable to add/remove from environment\n";
@@ -46,7 +46,7 @@ char *error_path_126(data_shell *datash)
 {
 	int length;
 	char *ver_str, *error;
-	
+
 	ver_str = aux_itoa(datash->counter);
 	length = _strlen(datash->av[0]) + _strlen(ver_str);
 	length += _strlen(datash->args[0]) + 24;
